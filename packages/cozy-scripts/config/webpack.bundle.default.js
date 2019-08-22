30a17cf7e5ced4cca32323ccb74ae3b77624d946
Patrick Browne <pt.browne@gmail.com>
2019-08-22 17:43:25 +0200

    chore: Add duplicate package checker plugin

--- packages/cozy-scripts/config/webpack.bundle.default.js
+++ packages/cozy-scripts/config/webpack.bundle.default.js
@@ -16,6 +16,7 @@ const configs = [
   require('./webpack.config.assets'),
   require('./webpack.config.manifest'),
   require('./webpack.config.progress'),
+  require('./webpack.config.duplicates'),
   addAnalyzer ? require('./webpack.config.analyzer') : null,
   require('./webpack.config.services'),
   require(`./webpack.target.${target}`)
